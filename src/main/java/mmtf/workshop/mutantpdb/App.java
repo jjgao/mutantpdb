47a84043b13813b9354736dfbd98ff89cda793f0
Alexey Strokach <ostrokach@gmail.com>
2017-06-28 14:03:13 -0700

    WIP

--- src/main/java/mmtf/workshop/mutantpdb/App.java
+++ src/main/java/mmtf/workshop/mutantpdb/App.java
@@ -1,10 +1,15 @@
 package mmtf.workshop.mutantpdb;
 
-import mmtf.workshop.mutantpdb.io.DataLocationProvider;
-import mmtf.workshop.mutantpdb.io.DataProvider;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SaveMode;
+import mmtf.workshop.mutantpdb.mappers.AddrResidueToKey;
+import mmtf.workshop.mutantpdb.utils.SaprkUtils;
+import edu.sdsc.mmtf.spark.mappers.StructureToPolymerChains;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import edu.sdsc.mmtf.spark.io.MmtfReader;
+import org.rcsb.mmtf.api.StructureDataInterface;
+
+import java.util.Arrays;
+
 
 /**
  * Hello world!
@@ -14,21 +19,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        DataProvider provider = new DataProvider();
 
-        Dataset<Row> mutations = provider.getOncoKBMutations();
-        // Uniprot to PDB mapping
-        Dataset<Row> map = provider.getUniprotToPdbMapping();
+        String path = System.getProperty("MMTF_REDUCED");
+        if (path == null) {
+            System.err.println("Environment variable for Hadoop sequence file has not been set");
+            System.exit(-1);
+        }
 
-        Dataset<Row> mutationsOnStructures = mutations.join(map, mutations.col("Uniprot")
-                .equalTo(map.col("uniProtId"))
-                .and(mutations.col("Position").equalTo(map.col("uniProtPos"))))
-                .drop(map.col("uniProtId")).drop(map.col("uniProtPos"))
-                .orderBy(mutations.col("Uniprot"));
+       //SparkConf conf = new SparkConf().setMaster("local[*]").setAppName(App.class.getSimpleName());
+       //JavaSparkContext sc = new JavaSparkContext(conf);
 
-        mutationsOnStructures.write()
-                .mode(SaveMode.Overwrite)
-                .parquet(DataLocationProvider.getMutationsFileLocation());
+        SaprkUtils.stopSparkSession();
 
+        JavaSparkContext sc = SaprkUtils.getSparkContext();
+
+        chains = MmtfReader
+                .downloadMmtfFiles(Arrays.asList("4hhb"), sc)
+                .flatMapToPair(new StructureToPolymerChains())
+                .flatMapToPair(new AddrResidueToKey());
+                //.mapToPair(new FilterResidue());
+//        chains.foreach(data -> {
+//            System.out.println("model="+data._1() + " label=" + data._2());
+//        });
+
+    sc.close();
     }
 }
