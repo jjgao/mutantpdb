50628334ffae56fff1bb17463b9a0e2070015bfc
JJ Gao <jianjiong.gao@gmail.com>
2017-06-28 13:23:49 -0400

    initial

---
+++ src/main/java/mmtf/workshop/mutantpdb/App.java
@@ -0,0 +1,13 @@
+package mmtf.workshop.mutantpdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
